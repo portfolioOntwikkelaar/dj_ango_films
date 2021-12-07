9086652cda8b7f3ba0e77e3b67c3d962ce6c1cf9
noordseoostende <voropayev1@mail.ru>
2021-12-07 20:57:35 +0100

    check pblsh_state

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,5 @@
+
+
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
@@ -5,18 +7,21 @@ from django.utils.text import slugify
 #Create your models here.
 from djangoflix.db.models import PublishStateOptions
 from djangoflix.db.receivers import publish_state_pre_save, slugify_pre_save
+from videos.models import Video
 
 
 
 
-class VideoQuerySet(models.QuerySet):
+class PlaylistQuerySet(models.QuerySet):
   def published(self):
     now = timezone.now()
     return self.filter( state=PublishStateOptions.PUBLISH, publish_timestamp__lte= now
     )
-class VideoManager(models.Manager):
+
+
+class PlaylistManager(models.Manager):
   def get_queryset(self):
-    return VideoQuerySet(self.model, using=self._db)
+    return PlaylistQuerySet(self.model, using=self._db)
 
   def published(self):
     return self.get_queryset().published()
@@ -24,18 +29,18 @@ class VideoManager(models.Manager):
 
 
 
-class Video(models.Model):
+class Playlist(models.Model):
   title = models.CharField(max_length=220)
   description = models.TextField(blank=True, null=True)
   slug = models.SlugField(blank=True, null=True)
-  video_id = models.CharField(max_length=220, unique=True)
+  video = models.ForeignKey(Video, null=True, on_delete=models.SET_NULL)
   active = models.BooleanField(default=True)
   timestamp = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   state = models.CharField(max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
   publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
-  objects = VideoManager()
+  objects = PlaylistManager()
 
 
   @property
@@ -43,22 +48,11 @@ class Video(models.Model):
     return self.active
 
   
-class VideoAllProxy(Video):
-  class Meta:
-    proxy = True
-    verbose_name = 'All Video'
-    verbose_name_plural = 'All Videos'
 
-
-class VideoPublishedProxy(Video):
-  class Meta:
-    proxy = True
-    verbose_name = 'Published Video'
-    verbose_name_plural = 'Published Videos'
 
   
 
-  pre_save.connect(publish_state_pre_save, sender=Video)
+  pre_save.connect(publish_state_pre_save, sender=Playlist)
 
 
-  pre_save.connect(slugify_pre_save, sender=Video)
+  pre_save.connect(slugify_pre_save, sender=Playlist)
--- playlists/tests.py
+++ playlists/tests.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 from django.utils.text import slugify
 
 from djangoflix.db.models import PublishStateOptions
-from .models import Video
+from .models import Playlist
 
-class VideoModelTestCase(TestCase):
+class PlaylistModelTestCase(TestCase):
   def setUp(self):
-    self.obj_a = Video.objects.create(title='This is my title', video_id='abc')
-    self.obj_b = Video.objects.create(title='This is my title', state=PublishStateOptions.PUBLISH, video_id='dabco')
+    self.obj_a = Playlist.objects.create(title='This is my title')
+    self.obj_b = Playlist.objects.create(title='This is my title', state=PublishStateOptions.PUBLISH)
 
   def test_slug_field(self):
     title = self.obj_a.title
@@ -17,28 +17,28 @@ class VideoModelTestCase(TestCase):
 
   def test_valid_title(self):
     title='This is my title'
-    qs = Video.objects.filter(title=title)
+    qs = Playlist.objects.filter(title=title)
     self.assertTrue(qs.exists())
   
   
   def test_created_count(self):
-    qs = Video.objects.all()
+    qs = Playlist.objects.all()
     self.assertEqual(qs.count(), 2)
 
   def test_draft_case(self):
-    qs = Video.objects.filter(state=PublishStateOptions.DRAFT)
+    qs = Playlist.objects.filter(state=PublishStateOptions.DRAFT)
     self.assertEqual(qs.count(), 1)
   
   def test_publish_case(self):
-    qs = Video.objects.filter(state=PublishStateOptions.PUBLISH)
+    qs = Playlist.objects.filter(state=PublishStateOptions.PUBLISH)
     now = timezone.now()
-    published_qs = Video.objects.filter(
+    published_qs = Playlist.objects.filter(
       state=PublishStateOptions.PUBLISH,
       publish_timestamp__lte= now)
     self.assertTrue(published_qs.exists())
 
   def test_publish_manager(self):
-    published_qs = Video.objects.all().published()
-    published_qs_2 = Video.objects.all().published()
+    published_qs = Playlist.objects.all().published()
+    published_qs_2 = Playlist.objects.all().published()
     self.assertTrue(published_qs.exists())
     self.assertEqual(published_qs.count(), published_qs_2.count())
